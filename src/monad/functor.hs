04f600cb2dff8f0ce8031d8877266102970c5221
zjhmale <zjhmale@gmail.com>
2015-06-23 19:38:40 +0800

    finish the functor part

---
+++ src/monad/functor.hs
@@ -0,0 +1,24 @@
+class Functor f where
+  fmap :: (a -> b) -> f a -> f b
+
+--haskell中的functor是一个kind为* -> *的typeclass也就是说在要instance这个typeclass的时候必须是一个类型构造器也就是获取一个类型返回一个新类型的data 比如Maybe
+
+instance Functor Maybe where
+    fmap f (Just x) = Just (f x)
+    fmap f Nothing = Nothing
+
+--对于list来说 fmap的类型是
+
+fmap :: (a -> b) -> [a] -> [b]
+
+--而一般listfmap其实就是大多数FP语言中的map
+
+--functor law
+
+--Identity
+
+fmap id functor = id functor
+
+--Composition
+
+fmap (f . g) functor = fmap f (fmap g functor)
